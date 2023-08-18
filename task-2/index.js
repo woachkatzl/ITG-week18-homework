2af50051ce474e7659d42560cb5e327a03778293
woachkatzl <y.v.polonskaya@gmail.com>
2023-08-17 20:38:16 -0400

    engaged localStorage to keep changes

--- task-2/index.js
+++ task-2/index.js
@@ -3,10 +3,21 @@ const taskInput = document.querySelector("#task-input");
 const addBtn = document.querySelector("#add-btn");
 
 const tasksContainer = document.querySelector(".tasks-list");
-const noTasksMsg = document.querySelector(".tasks-list p");
 const clearBtn = document.querySelector("#clear-btn");
 
+let dataToSave = [];
+
+//CLASSES
+class taskItem {
+    constructor(index, value) {
+        this.index = index;
+        this.value = value;
+    }
+}
+
 //FUNCTIONS
+
+//CALLBACK FUNCTIONS
 const createTask = (value, index) => {
 
     const newTaskElement = document.createElement("div");
@@ -28,26 +39,6 @@ const createTask = (value, index) => {
     return newTaskElement;
 };
 
-const addNewTask = () => {
-
-    const newTask = taskInput.value;
-    if (!newTask) {
-        alert('Чтобы добавить задачу, введите её в поле ввода.');
-        return;
-    }
-
-    if (noTasksMsg)
-        noTasksMsg.remove();
-
-    const newTaskIndex = tasksContainer.childElementCount + 1; // Каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
-
-    const newTaskElement = createTask(newTask, newTaskIndex);
-    tasksContainer.append(newTaskElement);
-
-    taskInput.value = null;
-    clearBtn.removeAttribute("disabled");
-};
-
 const createNoTasksMsg = () => {
 
     const noTasksMsg = document.createElement("p");
@@ -57,21 +48,81 @@ const createNoTasksMsg = () => {
     return noTasksMsg;
 };
 
-const clearTasks = () => {
-
+const removeAllTasks = () => {
     const allTasks = tasksContainer.children; //Получаем коллекцию всех элементов с задачами
     const allTasksArray = Array.from(allTasks); //транформируем их в массив, чтобы применять методы массивов с ожидаемым поведением
 
     for (let task of allTasksArray) {
         task.remove(); //Удаляем каждый элемент полученного массива
     }
+};
+
+const saveData = (index, value) => {
+    const taskInfo = new taskItem(index, value);
+    dataToSave.push(taskInfo);
+    localStorage.setItem("tasks", JSON.stringify(dataToSave));
+};
+
+//PRIMARY FUNCTIONS
+const addNewTask = () => {
+
+    const newTask = taskInput.value;
+    if (!newTask) {
+        alert('Чтобы добавить задачу, введите её в поле ввода.');
+        return;
+    }
+
+    const noTasksMsg = document.querySelector(".tasks-list p");
+    if (noTasksMsg)
+        noTasksMsg.remove();
+
+    const newTaskIndex = tasksContainer.childElementCount + 1; // Каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
+
+    const newTaskElement = createTask(newTask, newTaskIndex);
+    tasksContainer.append(newTaskElement);
+
+    saveData(newTaskIndex, newTask);
+
+    taskInput.value = null;
+    clearBtn.removeAttribute("disabled");
+};
+
+const clearTasks = () => {
+
+    removeAllTasks();
 
     const noTaskMsg = createNoTasksMsg();
     tasksContainer.append(noTaskMsg);
 
     clearBtn.setAttribute("disabled", "true");
+
+    localStorage.removeItem("tasks");
+    dataToSave = [];
+};
+
+const restoreSession = () => {
+
+    const storedData = localStorage.getItem("tasks");
+
+    if (!storedData) {
+        const noTaskMsg = createNoTasksMsg();
+        tasksContainer.append(noTaskMsg);
+
+        clearBtn.setAttribute("disabled", "true");
+    } else {
+        dataToSave = JSON.parse(storedData);
+
+        for (let task of dataToSave) {
+            const newTaskElement = createTask(task.value, task.index);
+            tasksContainer.append(newTaskElement);
+        }
+
+        clearBtn.removeAttribute("disabled");
+    }
 };
 
 // EVENT LISTENERS
 addBtn.addEventListener("click", addNewTask);
 clearBtn.addEventListener("click", clearTasks);
+
+window.addEventListener("DOMContentLoaded", restoreSession);
