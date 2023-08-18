4434beb094b43ec6fd0c42beb3f2e0d64efe83ff
woachkatzl <y.v.polonskaya@gmail.com>
2023-08-18 01:16:07 -0400

    enabled savinig checked status of tasks

--- task-2/index.js
+++ task-2/index.js
@@ -12,6 +12,7 @@ class taskItem {
     constructor(index, value) {
         this.index = index;
         this.value = value;
+        this.checked = false;
     }
 }
 
@@ -25,11 +26,11 @@ const createTask = (value, index) => {
 
     const newTaskInput = document.createElement("input");
     newTaskInput.setAttribute("type", "checkbox");
-    newTaskInput.setAttribute("id", `task${index}`); // каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
+    newTaskInput.setAttribute("id", index); // каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
     newTaskInput.classList.add("form-check-input", "me-0");
 
     const newTaskLabel = document.createElement("label");
-    newTaskLabel.setAttribute("for", `task${index}`);
+    newTaskLabel.setAttribute("for", index);
     newTaskLabel.classList.add("form-check-label", "flex-grow-1", "ms-3", "text-start");
     newTaskLabel.textContent = value;
 
@@ -63,6 +64,20 @@ const saveData = (index, value) => {
     localStorage.setItem("tasks", JSON.stringify(dataToSave));
 };
 
+const saveCheckedStatus = (event) => {
+    const task = event.target;
+    const taskId = task.getAttribute("id");
+    const savedTaskInfo = dataToSave.find(task => task.index === taskId);
+
+    if (task.checked) {
+        savedTaskInfo.checked = true;
+    } else {
+        savedTaskInfo.checked = false;
+    }
+
+    localStorage.setItem("tasks", JSON.stringify(dataToSave));
+};
+
 //PRIMARY FUNCTIONS
 const addNewTask = () => {
 
@@ -76,12 +91,13 @@ const addNewTask = () => {
     if (noTasksMsg)
         noTasksMsg.remove();
 
-    const newTaskIndex = tasksContainer.childElementCount + 1; // Каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
+    const newTaskIndex = "task" + (tasksContainer.childElementCount + 1); // Каждому новому элементу с задачей будет присвоен id с числом на одно больше, чем текущее количество задач в контейнере
 
     const newTaskElement = createTask(newTask, newTaskIndex);
     tasksContainer.append(newTaskElement);
 
     saveData(newTaskIndex, newTask);
+    newTaskElement.addEventListener("change", saveCheckedStatus);
 
     taskInput.value = null;
     clearBtn.removeAttribute("disabled");
@@ -115,6 +131,10 @@ const restoreSession = () => {
         for (let task of dataToSave) {
             const newTaskElement = createTask(task.value, task.index);
             tasksContainer.append(newTaskElement);
+            newTaskElement.addEventListener("change", saveCheckedStatus);
+
+            const elementcheckbox = newTaskElement.firstElementChild;
+            elementcheckbox.checked = task.checked;
         }
 
         clearBtn.removeAttribute("disabled");
